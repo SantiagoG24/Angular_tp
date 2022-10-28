387893d328ad219661fbb5c0bcc00544e9cf8668
Santiago Gonzalez <SantiagoGonzalez_2002@hotmail.com>
2022-10-28 09:54:03 -0300

    agregue routeo a la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChamberCervezasComponent } from './chamber-cervezas/chamber-cervezas.component';
+import { ChamberContactosComponent } from './chamber-contactos/chamber-contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo :'cervezas',
+    pathMatch:'full'
+  },
+  {
+    path:'cervezas',
+    component: ChamberCervezasComponent
+  },
+  {
+    path:'contactos',
+    component: ChamberContactosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
